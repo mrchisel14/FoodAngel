4fe9170ea858ceb6541b75b872a7497e2a6d9c48
Ricardo <r.noriega31@gmail.com>
2015-07-16 18:17:16 -0500

    Google Play Services

--- app/src/main/java/com/cop4656/teamdns/foodangel/ScannerActivity.java
+++ app/src/main/java/com/cop4656/teamdns/foodangel/ScannerActivity.java
@@ -5,14 +5,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
 
 
 public class ScannerActivity extends Activity {
+
+    final int RQS_GooglePlayServices = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scanner);
-    }
 
+        /* Checks if device has Google Play Services activated */
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
+
+        if (resultCode == ConnectionResult.SUCCESS) {
+            Toast.makeText(getApplicationContext(),
+                    "isGooglePlayServicesAvailable SUCCESS",
+                    Toast.LENGTH_LONG).show();
+        } else
+            GooglePlayServicesUtil.getErrorDialog(resultCode, this, RQS_GooglePlayServices).show();
+        /* --- */
+    }
 }
